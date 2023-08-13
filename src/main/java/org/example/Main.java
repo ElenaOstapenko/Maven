497cbac9aafe1186dafeaa8c11ceed8cca4bf757
Elena Ostapenko <e.o.86@yandex.ru>
2023-08-13 15:41:31 +0300

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+
+        int expected = 150;
+        long actual = service.calculate(5000, true);
+        System.out.println("1. " + expected + " == ? == " + actual);
+
+        expected = 50;
+        actual = service.calculate(5000, false);
+        System.out.println("2. " + expected + " == ? == " + actual);}
+    }
+}
